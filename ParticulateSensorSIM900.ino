5fca1d834f1acab9f5feaa734e15c04cbb6c59a2
alcomposer <alcomposer@gmail.com>
2015-04-19 21:01:25 +0930

    Thingspeak post working!

--- ParticulateSensorSIM900.ino
+++ ParticulateSensorSIM900.ino
@@ -2,7 +2,6 @@
 #include <SoftwareSerial.h>
 #include "inetGSM.h"
 
-
 //To change pins for Software Serial, use the two lines in GSM.cpp.   SIMCOM SIM900 uses Pin 7 & 8
 //To change pins for Poweron & Reset (effects begin()), change the lines in GSM.h
 
@@ -15,159 +14,142 @@
 //Updated by Alexander Waite Mitchell
 //SDS011 Dust Sensor Reader & Update to Thingspeak server over GSM
 
-InetGSM inet;
 
+
+InetGSM inet;
 
 char msg[50];
 int numdata;
 char inSerial[50];
-int i=0;
-boolean started=false;
-int dataToSend=30;
+int i = 0;
+boolean started = false;
+long feedId = 65329;
+char datastreamId[] = "1";
+int dataToSend = 0;
 
 // ThingSpeak Settings
-
 char thingSpeakAddress[] = "api.thingspeak.com";
-const int updateThingSpeakInterval = 16 * 1000; 
-char apiKey[] = "put_your_api_key_here";
+char writeAPIKey[] = "you_KEY_here";
+const int updateThingSpeakInterval = 16 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
+char sentMsg[50];
+
 
 
 void setup()
 {
   //Serial connection.
+  pinMode(9, OUTPUT);
+
+
   Serial.begin(9600);
   Serial.println("GSM Shield testing.");
+  startupGSM900();
   //Start configuration of shield with baudrate.
   //For http uses is raccomanded to use 4800 or slower.
-  if (gsm.begin(2400)){
+
+}
+
+void thingspeakPost() {
+  
+  char itoaBuffer[8];
+  char end_c[2];
+  end_c[0] = 0x1a;
+  end_c[1] = '\0';
+  char data[4];
+  String str;
+
+  str = String(dataToSend);
+  str.toCharArray(data, 4);
+
+
+if (inet.connectTCP(thingSpeakAddress, 80)) {
+
+    Serial.println("connected to thingspeak");
+
+    gsm.SimpleWrite("POST /update HTTP/1.1\r\n");
+    gsm.SimpleWrite("Host: api.thingspeak.com\r\n");
+    gsm.SimpleWrite("Connection: close\r\n");
+    gsm.SimpleWrite("X-THINGSPEAKAPIKEY: ");
+    gsm.SimpleWrite(writeAPIKey);
+    gsm.SimpleWrite("\r\n");
+    gsm.SimpleWrite("Content-Type: application/x-www-form-urlencoded\r\n");
+    gsm.SimpleWrite("Content-Length: ");
+    sprintf(sentMsg, "field1=%s", data);
+    itoa(strlen(sentMsg), itoaBuffer, 10);
+    gsm.SimpleWrite(itoaBuffer);
+    gsm.SimpleWrite("\r\n\r\n");
+    gsm.SimpleWrite(sentMsg);
+    gsm.SimpleWrite("\r\n\r\n");
+    gsm.SimpleWrite(end_c);
+    
+    Serial.print("sent message " );
+    Serial.print(sentMsg);
+    Serial.print(" ");
+    Serial.print(itoaBuffer);
+
+  }
+  else
+  {
+    Serial.println("failed");
+    startupGSM900();
+  }
+}
+
+
+void restartGSM900()
+{
+  digitalWrite(9, LOW);
+  delay(1000);
+  digitalWrite(9, HIGH);
+  delay(3000);
+  digitalWrite(9, LOW);
+  delay(3000);
+}
+
+void startupGSM900() {
+  if (gsm.begin(2400)) {
     Serial.println("\nstatus=READY");
-    started=true;
+    started = true;
   }
   else Serial.println("\nstatus=IDLE");
-  
-  if(started){
-     gsm.SimpleWrite("AT");
-     delay(1000);
-     gsm.WhileSimpleRead();
+
+  if (started) {
+    gsm.SimpleWrite("AT");
+    delay(1000);
+    gsm.WhileSimpleRead();
     //GPRS attach, put in order APN, username and password.
     //If no needed auth let them blank.
-    
-    if (inet.attachGPRS("put_your_APN_here", "", ""))
+
+    if (inet.attachGPRS("internet", "", ""))
       Serial.println("status=ATTACHED");
     else Serial.println("status=ERROR");
     delay(1000);
-    
-  
+
+
     //Read IP address.
     gsm.SimpleWriteln("AT+CIFSR");
+
+
+
     delay(5000);
+
     //Read until serial buffer is empty.
     gsm.WhileSimpleRead();
-  
-    //TCP Client GET, send a GET request to the server and
-    //save the reply.
-   // numdata=inet.httpPOST("www.cosm.com", 80, "/", msg, 50);
-    //Print the results.
-    /*Serial.println("\nNumber of data received:");
-    Serial.println(numdata);  
-    Serial.println("\nData received:"); 
-    Serial.println(msg);
-   */
-   };
-  //
-}
-void updateThingSpeak(String dataToSend)
-{
-if(inet.connectTCP(thingSpeakAddress, 80))
-  {
 
-    gsm.SimpleWrite("POST /update HTTP/1.1\n");
-    gsm.SimpleWrite("Host: api.thingspeak.com\n");
-    gsm.SimpleWrite("Connection: close\n");
-    gsm.SimpleWrite("X-THINGSPEAKAPIKEY: "+APIKey+"\n");
-    gsm.SimpleWrite("Content-Type: application/x-www-form-urlencoded\n");
-    gsm.SimpleWrite("Host: api.cosm.com\n");
-    gsm.SimpleWrite("Content-Length: ");
-    gsm.SimpleWrite(dataToSend.length());
-    gsm.SimpleWrite("\n\n");
-    gsm.SimpleWrite(dataToSend);
-  
- lastConnectionTime = millis();
-if (client.connected())
-{
-Serial.println("Connecting to ThingSpeak...");
-Serial.println();
-failedCounter = 0;
-}
-else
-{
-failedCounter++;
-Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");
-Serial.println();
-}
-}
-else
-{
-failedCounter++;
-Serial.println("Connection to ThingSpeak Failed ("+String(failedCounter, DEC)+")");
-Serial.println();
-lastConnectionTime = millis();
-}
+  }
+  else {
+    restartGSM900();
+  }
 }
 
 
-   
-  
-/*  else{ 
-    
-    Serial.println("Nonn connesso!!!");
-    
-  }*/
 
 
 
 void loop()
 {
-  //Read for new byte on serial hardware,
-  //and write them on NewSoftSerial.
- // serialhwread();
-  //Read for new byte on NewSoftSerial.
-  // serialswread();
+  thingspeakPost();
+  delay(15000);
+  dataToSend += 100;
 };
 
-void serialhwread(){
-  i=0;
-  if (Serial.available() > 0){
-    while (Serial.available() > 0) {
-      inSerial[i]=(Serial.read());
-      delay(10);
-      i++;      
-    }
-    
-    inSerial[i]='\0';
-    if(!strcmp(inSerial,"/END")){
-      Serial.println("_");
-      inSerial[0]=0x1a;
-      inSerial[1]='\0';
-      gsm.SimpleWriteln(inSerial);
-    }
-    //Send a saved AT command using serial port.
-    if(!strcmp(inSerial,"TEST")){
-      Serial.println("SIGNAL QUALITY");
-      gsm.SimpleWriteln("AT+CSQ");
-    }
-    //Read last message saved.
-    if(!strcmp(inSerial,"MSG")){
-      Serial.println(msg);
-    }
-    else{
-      Serial.println(inSerial);
-      gsm.SimpleWriteln(inSerial);
-    }    
-    inSerial[0]='\0';
-  }
-}
-
-void serialswread(){
-  gsm.SimpleRead();
-}
