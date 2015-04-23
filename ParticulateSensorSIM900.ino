ba07fdbe78d3bf2e2c1562113779feb22586a289
alcomposer <alcomposer@users.noreply.github.com>
2015-04-23 14:47:35 +0930

    No longer using SoftSerial because of MEGA

--- ParticulateSensorSIM900.ino
+++ ParticulateSensorSIM900.ino
@@ -2,21 +2,20 @@
 #include <SoftwareSerial.h>
 #include "inetGSM.h"
 
-//To change pins for Software Serial, use the two lines in GSM.cpp.   SIMCOM SIM900 uses Pin 7 & 8
-//To change pins for Poweron & Reset (effects begin()), change the lines in GSM.h
-
-//Original code from:
-//GSM Shield for Arduino
-//www.open-electronics.org
-//this code is based on the example of Arduino Labs.
-//Simple sketch to start a connection as client.
-
-//Updated by Alexander Waite Mitchell
-//SDS011 Dust Sensor Reader & Update to Thingspeak server over GSM
-
-
 
 InetGSM inet;
+
+
+//dust sensor stuff
+//SoftwareSerial dustSerial(10,11);  //RX,  TX
+
+
+unsigned int Pm25 = 0;
+unsigned int Pm10 = 0;
+//end dust sensor stuff
+
+
+
 
 char msg[50];
 int numdata;
@@ -25,12 +24,13 @@ int i = 0;
 boolean started = false;
 long feedId = 65329;
 char datastreamId[] = "1";
-int dataToSend = 0;
+//int dataToSend25 = 0;
+//int dataToSend10 = 0;
 
 // ThingSpeak Settings
 char thingSpeakAddress[] = "api.thingspeak.com";
-char writeAPIKey[] = "you_KEY_here";
-const int updateThingSpeakInterval = 16 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
+char writeAPIKey[] = "HF3Y3SP6GU2V9LB2";
+const int updateThingSpeakInterval = 60 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
 char sentMsg[50];
 
 
@@ -40,31 +40,49 @@ void setup()
   //Serial connection.
   pinMode(9, OUTPUT);
 
+Serial2.begin(9600);
+Serial.begin(9600);
+Serial.println("GSM Shield testing.");
+ 
 
-  Serial.begin(9600);
-  Serial.println("GSM Shield testing.");
-  startupGSM900();
+
+startupGSM900();
+    
+    //PM Sensor setup
+    //dustSerial.begin(9600);
+    Pm25=0;
+    Pm10=0;
   //Start configuration of shield with baudrate.
   //For http uses is raccomanded to use 4800 or slower.
 
 }
 
 void thingspeakPost() {
+
+  
   
   char itoaBuffer[8];
+
   char end_c[2];
   end_c[0] = 0x1a;
   end_c[1] = '\0';
-  char data[4];
-  String str;
 
-  str = String(dataToSend);
-  str.toCharArray(data, 4);
+// PM25 int to char
+  char data25[50];
+  String str25;
+  str25 = String(Pm25);
+  str25.toCharArray(data25, 50);
+  
+// PM10 int to char  
+  char data10[50];
+  String str10;
+  str10 = String(Pm10);
+  str10.toCharArray(data10, 50);
 
 
 if (inet.connectTCP(thingSpeakAddress, 80)) {
 
-    Serial.println("connected to thingspeak");
+   Serial.println("connected to thingspeak");
 
     gsm.SimpleWrite("POST /update HTTP/1.1\r\n");
     gsm.SimpleWrite("Host: api.thingspeak.com\r\n");
@@ -74,23 +92,28 @@ if (inet.connectTCP(thingSpeakAddress, 80)) {
     gsm.SimpleWrite("\r\n");
     gsm.SimpleWrite("Content-Type: application/x-www-form-urlencoded\r\n");
     gsm.SimpleWrite("Content-Length: ");
-    sprintf(sentMsg, "field1=%s", data);
+    sprintf(sentMsg, "field1=%s&field2=%s", data25, data10);
     itoa(strlen(sentMsg), itoaBuffer, 10);
+
     gsm.SimpleWrite(itoaBuffer);
+
     gsm.SimpleWrite("\r\n\r\n");
+
     gsm.SimpleWrite(sentMsg);
+
     gsm.SimpleWrite("\r\n\r\n");
+
     gsm.SimpleWrite(end_c);
-    
-    Serial.print("sent message " );
-    Serial.print(sentMsg);
-    Serial.print(" ");
-    Serial.print(itoaBuffer);
+
+  //  Serial.print("sent message " );
+ //   Serial.print(sentMsg);
+ //   Serial.print(" ");
+ //   Serial.print(itoaBuffer);
 
   }
   else
   {
-    Serial.println("failed");
+Serial.println("failed");
     startupGSM900();
   }
 }
@@ -107,11 +130,14 @@ void restartGSM900()
 }
 
 void startupGSM900() {
-  if (gsm.begin(2400)) {
-    Serial.println("\nstatus=READY");
+  
+  
+  
+  if (gsm.begin(115200)) {
+Serial.println("\nstatus=READY");
     started = true;
   }
-  else Serial.println("\nstatus=IDLE");
+else Serial.println("\nstatus=IDLE");
 
   if (started) {
     gsm.SimpleWrite("AT");
@@ -121,7 +147,7 @@ void startupGSM900() {
     //If no needed auth let them blank.
 
     if (inet.attachGPRS("internet", "", ""))
-      Serial.println("status=ATTACHED");
+    Serial.println("status=ATTACHED");
     else Serial.println("status=ERROR");
     delay(1000);
 
@@ -137,19 +163,85 @@ void startupGSM900() {
     gsm.WhileSimpleRead();
 
   }
-  else {
-    restartGSM900();
-  }
+  
 }
 
 
+void ProcessSerialData()
+{
+  
+  
+  uint8_t mData = 0;
+  uint8_t i = 0;
+  uint8_t mPkt[10] = {0};
+  uint8_t mCheck = 0;
+ while (Serial2.available() > 0) 
+  {  
+    // from www.inovafitness.com
+    // packet format: AA C0 PM25_Low PM25_High PM10_Low PM10_High 0 0 CRC AB
+     mData = Serial2.read();     delay(2);//wait until packet is received
+    if(mData == 0xAA)//head1 ok
+     {
+        mPkt[0] =  mData;
+        mData = Serial2.read();
+        if(mData == 0xc0)//head2 ok
+        {
+          mPkt[1] =  mData;
+          mCheck = 0;
+          for(i=0;i < 6;i++)//data recv and crc calc
+          {
+             mPkt[i+2] = Serial2.read();
+             delay(2);
+             mCheck += mPkt[i+2];
+          }
+          mPkt[8] = Serial2.read();
+          delay(1);
+	  mPkt[9] = Serial2.read();
+          if(mCheck == mPkt[8])//crc ok
+          {
+           Serial2.flush();
+           // dustSerial.write(mPkt,10);
 
+            Pm25 = (uint16_t)mPkt[2] | (uint16_t)(mPkt[3]<<8);
+            Pm10 = (uint16_t)mPkt[4] | (uint16_t)(mPkt[5]<<8);
+            if(Pm25 > 9999)
+             Pm25 = 9999;
+            if(Pm10 > 9999)
+             Pm10 = 9999;            
+            //get one good packet
+             return;
+          }
+        }      
+     }
+   } 
+}
 
 
 void loop()
 {
+
+  ProcessSerialData();
+
   thingspeakPost();
-  delay(15000);
-  dataToSend += 100;
+  Serial.print("PM2.5 is: ");
+  Serial.println(Pm25);
+ Serial.print("PM10 is: ");
+ Serial.println(Pm10);
+  delay(60000);
+  
+  
+
+
+
+  //if (dataToSend > 500) {
+  //  restartGSM900();
+    //dataToSend = 0;
+ // }
+
+  //Read for new byte on serial hardware,
+  //and write them on NewSoftSerial.
+  //serialhwread();
+  //Read for new byte on NewSoftSerial.
+  // serialswread();
 };
 
